1f076966c821ae97b426cf2a967821ffc9300035
NathaliaMoraesSilva <nathaliamoraes28@hotmail.com>
2025-09-09 23:54:44 +0000

    iniciando atv

---
+++ atividade-POO classes e objetos.py
@@ -0,0 +1,8 @@
+class pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        
+        
+pessoa01 = pessoa("nathalia","23")
+print(pessoa01.nome, pessoa01.idade)
