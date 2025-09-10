df1050265600cb61f8d1174936196d588951527c
NathaliaMoraesSilva <nathaliamoraes28@hotmail.com>
2025-09-10 20:33:32 -0300

    Iniciando atividade

--- atividade-POO classes e objetos.py
+++ atividade-POO classes e objetos.py
@@ -1,8 +1,13 @@
 class pessoa:
-    def __init__(self, nome, idade):
+    def __init__ (self, nome, idade):
         self.nome = nome
         self.idade = idade
-        
-        
-pessoa01 = pessoa("nathalia","23")
-print(pessoa01.nome, pessoa01.idade)
+
+
+    def apresentacao (self):
+        return f"olá meu {self.nome} e tenho {self.idade}"
+
+pessoa1 = pessoa("pedro", 23)
+pessoa2 = pessoa("Joao", 24)
+print(pessoa1.apresentacao())
+print(pessoa2.aprese)
